7af335028a9096ea652f18c8357061b661d3c45f
Kevin Wallace <kevin@pentabarf.net>
2014-03-23 00:23:47 -0700

    Add limits on execution time (15s), output size (1 MiB)

--- shell/main.go
+++ shell/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"os/exec"
+	"time"
 )
 
 func main() {
@@ -28,7 +31,9 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
         <form id="cmdform">
           <input id="cmd" />
         </form>
-        <pre id="output"></pre>
+        <pre id="output">Type a shell command in the above textbox and press enter to execute.
+
+Yes, DoS attacks are possible.  Please be nice while Sandstorm is still in alpha.</pre>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
         <script>
           $("#cmdform").submit(function(e) {
@@ -50,9 +55,50 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 func handleCommand(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-type", "text/plain")
 	cmd := exec.Command("/bin/sh", "-c", r.FormValue("cmd"))
-	output, err := cmd.CombinedOutput()
-	w.Write(output)
-	if err != nil {
+	out := &limitWriter{w, 1024 * 1024} // 1 MiB
+	cmd.Stdout = out
+	cmd.Stderr = out
+	if err := runWithTimeout(cmd, 10*time.Second); err != nil {
 		fmt.Fprintf(w, "\n%s", err)
 	}
+}
+
+func runWithTimeout(cmd *exec.Cmd, d time.Duration) error {
+	errch := make(chan error, 1)
+	go func() {
+		errch <- cmd.Run()
+	}()
+	select {
+	case err := <-errch:
+		return err
+	case <-time.After(d):
+		cmd.Process.Kill()
+		return fmt.Errorf("timed out after %s", d)
+	}
+}
+
+// limitWriter wraps an io.Writer, only allowing a limited number of bytes to be
+// written before returning errors.
+type limitWriter struct {
+	w io.Writer // underlying writer
+	n int       // number of bytes remaining
+}
+
+func (w *limitWriter) Write(p []byte) (n int, err error) {
+	var trunc bool
+	if len(p) > w.n {
+		p = p[:w.n]
+		trunc = true
+	}
+	if len(p) > 0 {
+		n, err = w.w.Write(p)
+		w.n -= n
+		if w.n == 0 {
+			w.w.Write([]byte("\n...output truncated"))
+		}
+	}
+	if trunc && err != nil {
+		err = errors.New("output truncated")
+	}
+	return
 }
